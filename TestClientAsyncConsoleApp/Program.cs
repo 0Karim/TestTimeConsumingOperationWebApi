e17ba62a66d5e93dd317bd10509cb1f5a5e6c461
Karim Alaa <karimalaa298@gmail.com>
2021-05-12 00:39:11 +0200

    Add implementation to call the web api in console app

--- TestClientAsyncConsoleApp/Program.cs
+++ TestClientAsyncConsoleApp/Program.cs
@@ -1,12 +1,58 @@
 ﻿using System;
+using System.Net.Http;
 
 namespace TestClientAsyncConsoleApp
 {
     class Program
     {
+        static int _localOperationCounter = 0;
+        static int _webAPIOperationCounter = 0;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Click Local press 1");
+            Console.WriteLine("Click Local press 2");
+
+            var input = Console.ReadLine();
+            while (input != "0")
+            {
+                if (input == "1")
+                    LocalOperation();
+
+                if (input == "2")
+                    WebAPICall_Operation();
+
+                input = Console.ReadLine();
+            }
+
+            Console.ReadLine();
+
+        }
+
+        public static void LocalOperation()
+        {
+            _localOperationCounter++;
+            AddTextToConsoleScreen($"Fast Local Operation Completed {_localOperationCounter}");
+        }
+
+        public static async void WebAPICall_Operation()
+        {
+            HttpClient httpClient = new HttpClient();
+
+            HttpResponseMessage httpResponseMessage = await httpClient.GetAsync("http://localhost:62833/TestLongOperation");
+
+            string result = await httpResponseMessage.Content.ReadAsStringAsync();
+
+            _webAPIOperationCounter++;
+
+            AddTextToConsoleScreen($"{result} {_webAPIOperationCounter}");
+
+
+        }
+
+        private static void AddTextToConsoleScreen(string text)
+        {
+            Console.WriteLine(text);
         }
     }
 }
---
+++ TestTimeConsumingOperationWebApi/Controllers/TestLongOperationController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TestTimeConsumingOperationWebApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class TestLongOperationController : ControllerBase
+    {
+        /// <summary>
+        /// This is code forming a long running IO operation to work with async and await programming
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            await Task.Delay(15000);
+
+            return Content("Web API Long Operation Completed");
+        }
+    }
+}
