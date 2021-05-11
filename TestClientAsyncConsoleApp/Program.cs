e64134d5d17309f6e6911ae7116d39ba55eb758c
Karim Alaa <karimalaa298@gmail.com>
2021-05-12 00:08:46 +0200

    Add project files.

---
+++ TestClientAsyncConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestClientAsyncConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
